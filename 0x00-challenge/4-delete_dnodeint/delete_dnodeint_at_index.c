a023275fad7ca8ee5726fa11447f50bfbb324db1
Samuel <ogboyesam@gmail.com>
2023-08-21 19:12:44 -0700

    delete node modified

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -31,7 +31,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		*head = saved_head;
 		return (-1);
 	}
-	if (index == 0)
+	if (0 == index)
 	{
 		tmp = (*head)->next;
 		free(*head);
@@ -43,12 +43,11 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	else
 	{
-		 tmp = (*head)->next;
-		 free(*head);
-		 tmp->prev = (*head)->prev;
-		 if (tmp != NULL)
-			 tmp->prev = (*head)->prev;
-		 *head = tmp;
+		(*head)->prev->prev = (*head)->prev;
+		free(*head);
+		if ((*head)->next)
+			(*head)->prev->next = (*head)->next;
+		*head = saved_head;
 	}
 	return (1);
 }
